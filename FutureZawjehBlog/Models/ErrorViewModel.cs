b7fe06d744d863446393e91d9c2b0a6bb4b9acc3
Khair Horani <khair04@outlook.com>
2024-11-20 11:43:00 +0300

    Add project files.

---
+++ FutureZawjehBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FutureZawjehBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
